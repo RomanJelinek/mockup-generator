4572dbf92509564d47a51d37a6a1418a878f6d21
RomanJelinek <roman.jelinek91@gmail.com>
2022-09-03 10:23:22 +0200

    trying html to dom

--- Components/Mockup1.tsx
+++ Components/Mockup1.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useCallback, useEffect, useState } from 'react';
 import * as htmlToImage from 'html-to-image';
-import html2canvas from 'html2canvas';
+import domtoimage from 'dom-to-image';
 
 export interface IAppProps {}
 
@@ -10,24 +10,27 @@ export const Mockup1: React.FC<any> = ({ url }) => {
   const [currentImage, setCurrentImage] = useState('');
 
   useEffect(() => {
-  //  url && onButtonClick();
+    url && onButtonClick();
   }, [url]);
 
   const onButtonClick = () => {
  
-        const printArea = document.getElementById('print');
+       var node = document.getElementById('print');
 
-        html2canvas(printArea).then((canvas) => {
-          const dataURL = canvas.toDataURL();
-          console.log(dataURL)
-          sendImg(dataURL)
-        });
+       domtoimage
+         .toPng(node)
+         .then(function (dataUrl) {
+           sendImg(dataUrl)
+         })
+         .catch(function (error) {
+           console.error('oops, something went wrong!', error);
+         });
 
   };
 
   return (
     <div style={{ display: 'flex', flex: '3' }}>
-      <div id="print" style={{ height: '600px', width: '532px' }}>
+      <div id="print"  style={{ height: '600px', width: '532px' }}>
         <img
           src="/img/mockups/mockup1black.png"
           style={{
@@ -43,7 +46,7 @@ export const Mockup1: React.FC<any> = ({ url }) => {
             width: '350px',
             top: '50px',
             left: '90px',
-            zIndex: '4',
+            zIndex: '-1',
           }}
           src={url}
         ></img>
