c24a42568241c20495aef09039705f2a8412a1f0
RomanJelinek <roman.jelinek91@gmail.com>
2022-09-03 13:05:41 +0200

    different way of handling api

--- pages/api/get-images.ts
+++ pages/api/get-images.ts
@@ -1,7 +1,17 @@
+import nextBase64 from 'next-base64';
+
 const handler = async (req: any, res: any) => {
   const result = await fetch(
-    `https://${process.env.API_KEY}:${process.env.SECRET_KEY}@api.cloudinary.com/v1_1/${process.env.NAME_URL}/resources/image?&max_results=500`
-  ).then((r) => r.json());
+    `https://api.cloudinary.com/v1_1/${process.env.NAME_URL}/resources/image`,
+    {
+      method: 'get',
+      headers: {
+        Authorization:
+          'Basic ' +
+          nextBase64.encode(process.env.API_KEY + ':' + process.env.SECRET_KEY),
+      },
+    }
+  ).then((res) => res.json());
 
   const filteredData = await result.resources.map((img) => {
     return { url: img.url, title: '', category: '' };
