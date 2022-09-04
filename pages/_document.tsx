203cc92a33dccdd3e52fbabc9589448552ba3c52
RomanJelinek <roman.jelinek91@gmail.com>
2022-09-04 10:46:49 +0200

    added smartlook

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -7,6 +8,16 @@ export default function Document() {
         <meta
           httpEquiv="Content-Security-Policy"
           content="upgrade-insecure-requests"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `  window.smartlook||(function(d) {
+    var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+    var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+    c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
+    })(document);
+    smartlook('init', '64d9faeeb23e5f6eec97d514038316afb2eac13f', { region: 'eu' });`,
+          }}
         />
       </Head>
       <body>
